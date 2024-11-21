24393cd53323aa26935eada8badfa712bb9449b7
Thalia <thalia.soler.7e7@itb.cat>
2024-11-21 17:34:46 +0100

    add deep:true

--- src/components/WatchArrayExample.vue
+++ src/components/WatchArrayExample.vue
@@ -1,8 +1,22 @@
+<!-- <script setup>
+import { ref, watch } from 'vue'
+const numbers = ref([])
+
+const text = ref('')
+watch(text, (newText, oldText) => console.log(newText, oldText))
+
+const randomNum = () => numbers.value.push(Math.random())
+</script>
+<template>
+  <input type="text" v-model="text"/>
+  <button @click="randomNum">Add Random Number</button>
+</template> -->
+
 <script setup>
 import { ref, watch } from 'vue'
 const numbers = ref([])
 
-watch(numbers.value, (newValue, oldValue) => console.log(newValue, oldValue))
+watch(numbers, (numbers) => console.log(numbers),{deep:true})
 
 const randomNum = () => numbers.value.push(Math.random())
 </script>
